354a40e4f26be2c7ba29f88a2bd12dbd0af02429
ericgiannini <eric.giannini@yahoo.com>
2017-04-18 16:16:25 -0700

    Updating .playground

--- s.Manipulator.playground/Contents.swift
+++ s.Manipulator.playground/Contents.swift
@@ -2,10 +2,23 @@
 
 import UIKit
 
-/*: 
+/*:
  
  # String Manipulation
  
  */
+
+/*:
+ 
+ Does a string have unique letters?
+ 
+ */
+
+func alphabeticallySingularString(input: String) -> Bool {
+    return Set(input.characters).count == input.characters.count
+}
+
+alphabeticallySingularString(input: "cannabis")
+
 
 
